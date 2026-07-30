b5c251ec7fded2bd0d5e8417fbd5a431dbad9dd6
Craig Fox <craig.fox@icloud.com>
2026-07-30 15:26:34 +1200

    Included checkstyle in order-service

--- order-service/src/main/java/nz/fox/craig/order/exception/ProductNotFoundException.java
+++ order-service/src/main/java/nz/fox/craig/order/exception/ProductNotFoundException.java
@@ -2,7 +2,7 @@ package nz.fox.craig.order.exception;
 
 import java.util.UUID;
 
-public class ProductNotFoundException extends RuntimeException{
+public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(UUID id) {
 		super(String.format( "Product %s not found", id));
 	}
