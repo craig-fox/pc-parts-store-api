17c2050b471146cb7dd1011a4d5967880062cd05
Craig Fox <craig.fox@icloud.com>
2026-08-02 15:29:09 +1200

    Added manual mapper for now to fix error

--- product-service/src/main/java/nz/fox/craig/product/mapper/ProductMapper.java
+++ product-service/src/main/java/nz/fox/craig/product/mapper/ProductMapper.java
@@ -2,14 +2,41 @@ package nz.fox.craig.product.mapper;
 
 import java.util.List;
 
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
 import nz.fox.craig.product.dto.ProductResponse;
 import nz.fox.craig.product.model.Product;
 
-@Mapper(componentModel = "spring")
-public interface ProductMapper {
+@Component
+public class ProductMapper implements ProductMapperGenerated {
 
-    ProductResponse toResponse(Product product);
+    @Override
+    public ProductResponse toResponse(Product product) {
+        if (product == null) {
+            return null;
+        }
 
-    List<ProductResponse> toResponseList(List<Product> products);
+        return new ProductResponse(
+                product.getId(),
+                product.getSku(),
+                product.getName(),
+                product.getDescription(),
+                product.getBrand(),
+                product.getCategory(),
+                product.getPrice(),
+                product.getStockQuantity(),
+                product.getWeightKg(),
+                product.getImageUrl());
+    }
+
+    @Override
+    public List<ProductResponse> toResponseList(List<Product> products) {
+        if (products == null) {
+            return null;
+        }
+
+        return products.stream()
+                .map(this::toResponse)
+                .toList();
+    }
 }
---
+++ product-service/src/main/java/nz/fox/craig/product/mapper/ProductMapperGenerated.java
@@ -0,0 +1,15 @@
+package nz.fox.craig.product.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import nz.fox.craig.product.dto.ProductResponse;
+import nz.fox.craig.product.model.Product;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapperGenerated {
+
+    ProductResponse toResponse(Product product);
+
+    List<ProductResponse> toResponseList(List<Product> products);
+}
