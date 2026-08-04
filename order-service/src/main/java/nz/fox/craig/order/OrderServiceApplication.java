80d5fbdf23e8a41473c5e39d203e47c4c408bf33
Craig Fox <craig.fox@icloud.com>
2026-08-04 18:05:31 +1200

    Minor pom changes

--- order-service/src/main/java/nz/fox/craig/order/OrderServiceApplication.java
+++ order-service/src/main/java/nz/fox/craig/order/OrderServiceApplication.java
@@ -1,13 +1,8 @@
 package nz.fox.craig.order;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import nz.fox.craig.order.mapper.OrderMapper;
 
 
 @SpringBootApplication(scanBasePackages = {
@@ -20,6 +15,5 @@ public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-
 
 }
