0e4172b5399e9cb66e1e74848fc192ccc5703ef0
Craig Fox <craig.fox@icloud.com>
2026-08-05 17:53:50 +1200

    Removed mapstruct and updated modules to use root pom

--- inventory-service/src/main/java/nz/fox/craig/inventory/dto/InventoryResponse.java
+++ inventory-service/src/main/java/nz/fox/craig/inventory/dto/InventoryResponse.java
@@ -3,8 +3,10 @@ package nz.fox.craig.inventory.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import lombok.Builder;
 import nz.fox.craig.inventory.model.InventoryStatus;
 
+@Builder
 public record InventoryResponse(
         UUID productId,
         int quantityOnHand,
--- inventory-service/src/main/java/nz/fox/craig/inventory/mapper/InventoryMapper.java
+++ inventory-service/src/main/java/nz/fox/craig/inventory/mapper/InventoryMapper.java
@@ -1,11 +1,29 @@
 package nz.fox.craig.inventory.mapper;
 
-import org.mapstruct.Mapper;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import nz.fox.craig.inventory.dto.InventoryResponse;
 import nz.fox.craig.inventory.model.Inventory;
 
-@Mapper(componentModel = "spring")
-public interface InventoryMapper {
+@Component
+public class InventoryMapper {
 
-    InventoryResponse toResponse(Inventory inventory);
+    public InventoryResponse toResponse(Inventory inventory) {
+        return InventoryResponse.builder()
+                .productId(inventory.getProductId())
+                .quantityOnHand(inventory.getQuantityOnHand())
+                .quantityReserved(inventory.getQuantityReserved())
+                .availableQuantity(inventory.getAvailableQuantity())
+                .lastUpdated(inventory.getLastUpdated())
+                .status(inventory.getStatus())
+                .build();
+    }
+
+    public List<InventoryResponse> toResponseList(List<Inventory> inventory) {
+        return inventory.stream()
+                .map(this::toResponse)
+                .toList();
+    }
 }
