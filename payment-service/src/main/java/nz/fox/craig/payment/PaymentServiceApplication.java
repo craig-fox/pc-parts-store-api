61f68fdf9fe4622ed0cb4e6827c51382ef11e2f4
Craig Fox <craig.fox@icloud.com>
2026-08-22 17:25:48 +1200

    Added security config for payment-service

--- payment-service/src/main/java/nz/fox/craig/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/nz/fox/craig/payment/PaymentServiceApplication.java
@@ -3,7 +3,7 @@ package nz.fox.craig.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = "nz.fox.craig")
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
